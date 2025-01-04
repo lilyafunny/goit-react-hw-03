721c812c9b2541a0c15b5e80ff39e76c76db3f56
Lili <lilyafunny21@gmail.com>
2025-01-04 17:41:38 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import ContactForm from "./components/ContactForm/ContactForm"
+import SearchBox from "./components/SearchBox/SearchBox"
+import ContactList from "./components/ContactList/ContactList"
+
+import { useState } from 'react'
+
+const App = () => {
+  const [number, setNumber] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]
+  );
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </>
+  )
+}
+
+export default App
